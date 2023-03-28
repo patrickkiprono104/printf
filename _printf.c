659c0224fa991d461d19c677af9636ec1cf1e497
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-03-28 01:41:26 -0700

    tried another approach to _printf.c

--- _printf.c
+++ _printf.c
@@ -1,66 +1,82 @@
 #include "main.h"
 
 int _putchar(char c);
+
 /**
  * _printf - prints formatted output to stdout
- *
  * @format: format string
- *
  * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, i;
+	int count = 0;
 
 	va_start(args, format);
 
-	while (*format != '\0')
+	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == 'c')
+			switch (*format)
 			{
-				int c = va_arg(args, int);
-
-				_putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char*);
-
-				for (i = 0; s[i] != '\0'; i++)
-				{
-					_putchar(s[i]);
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				_putchar('%');
-				count++;
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += _print_str(va_arg(args, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(*format);
+					break;
 			}
 		}
 		else
 		{
-			_putchar(*format);
-			count++;
+			count += _putchar(*format);
 		}
 		format++;
 	}
+
 	va_end(args);
+
 	return (count);
 }
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: on Success 1
+ * Return: On success 1.
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
+}
+
+/**
+ * _print_str - prints a string
+ * @str: the string to print
+ * Return: the number of characters printed
+ */
+int _print_str(char *str)
+{
+	int count = 0;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
+
+	return (count);
 }
